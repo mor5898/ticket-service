33b98b97c8d4508af659ca3c8879df7087db3291
jah7471 <jah7471@thi.de>
2024-04-14 18:59:36 +0200

    TicketService edited, added Update and Save method

---
+++ src/main/java/com/benevolo/repo/CustomerRepo.java
@@ -0,0 +1,10 @@
+package com.benevolo.repo;
+
+import com.benevolo.entity.TicketEntity;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class CustomerRepo implements PanacheRepository<CustomerEntity> {
+
+}
--- src/main/java/com/benevolo/service/TicketService.java
+++ src/main/java/com/benevolo/service/TicketService.java
@@ -4,8 +4,11 @@ import com.benevolo.dto.TicketDTO;
 import com.benevolo.entity.TicketEntity;
 import com.benevolo.mapper.TicketMapper;
 import com.benevolo.repo.TicketRepo;
+import com.benevolo.utils.TicketStatus
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+
+import java.util.UUID;
 
 import java.util.List;
 
@@ -23,4 +26,21 @@ public class TicketService {
         return TicketMapper.map(ticketRepo.findAll().stream().toList());
     }
 
+    public void updateTicket(String ticketID, TicketDTO ticketDTO) {
+        TicketEntity ticketEntity = ticketRepo.findByID(ticketID).orElseThrow();
+        ticketEntity.setId(ticketDTO.id());
+        ticketEntity.setStatus(ticketDTO.status());
+        ticketEntity.setPrice(ticketDTO.price());
+        ticketEntity.setTaxRate(ticketDTO.taxRate());
+        ticketEntity.setCustomer(ticketDTO.customer());
+        TicketRepo.save(ticketEntity);
+    }
+
+    public void saveTicket(TicketDTO ticketDTO) {
+        TicketEntity ticketEntity = TicketMapper.map(ticketDTO);
+        if(ticketDTO.id().isBlank()) {
+            ticketEntity.setId(UUID.randomUUID().toString());
+        }
+        TicketRepo.save(ticketEntity);
+    }
 }
