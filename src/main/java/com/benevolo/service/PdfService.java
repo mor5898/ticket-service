4bffac95bae81f53772c5c1760f93222b0214488
DoneJeere08 <ant0593@thi.de>
2024-05-07 18:28:28 +0200

    Design Änderung und Dateneinbindung

--- src/main/java/com/benevolo/service/PdfService.java
+++ src/main/java/com/benevolo/service/PdfService.java
@@ -12,6 +12,7 @@ import org.apache.pdfbox.cos.COSArray;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import org.apache.pdfbox.pdmodel.graphics.color.PDColor;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
@@ -66,7 +67,9 @@ public class PdfService {
                             qrCodeImage,
                             eventName,
                            eventId,
-                           // ticketType.getName());
+                           ticket.getPrice(),
+                           ticket.getId(),
+                           //ticketType.getName());
                             "Blabla");
                             count++;
 
@@ -82,12 +85,15 @@ public class PdfService {
     }
 
     private static void drawTicket(PDDocument document, PDPage page, PDType0Font font, PDImageXObject image, float startY, PDImageXObject qrCode,
-                                   String eventName, String eventId, String ticketTypeName) throws IOException {
+                                   String eventName, String eventId, int price, String ticketId, String ticketTypeName) throws IOException {
         try (PDPageContentStream contentStream = new PDPageContentStream(document, page, PDPageContentStream.AppendMode.APPEND, true)) {
 
             // Hintergrundbild für das Ticket
+            PDImageXObject bgImage = PDImageXObject.createFromFile("img/AdobeStock_456815540_Preview.jpeg", document);
+            contentStream.drawImage(bgImage, 0, startY - 240, PDRectangle.A4.getWidth(), 265);
+
             //PDImageXObject bgImage = PDImageXObject.createFromFile("img/_6de14522-d0db-464a-840b-853726a9bc7d.jpeg", document);
-            //contentStream.drawImage(bgImage, 0, startY, page.getMediaBox().getWidth(), page.getMediaBox().getHeight() / 3);
+            //contentStream.drawImage(bgImage, 0, startY, PDRectangle.A4.getWidth(), PDRectangle.A4.getHeight()/3 );
 
             // QR-Code
             contentStream.drawImage(qrCode, 500, startY - 80, 100, 100);
@@ -95,42 +101,42 @@ public class PdfService {
             //FestivalId
             contentStream.beginText();
             contentStream.setFont(font, 12);
-            contentStream.newLineAtOffset(350, startY - 130);
-            contentStream.showText(eventId);
+            contentStream.newLineAtOffset(350, startY - 200);
+            contentStream.showText(ticketId);
             contentStream.endText();
 
             // Festivalname links oben
             contentStream.beginText();
             contentStream.setFont(font, 22);
-            contentStream.newLineAtOffset(50, startY);
+            contentStream.newLineAtOffset(27, startY);
             contentStream.showText(eventName);
             contentStream.endText();
 
             // Ort des Festivals
             contentStream.beginText();
             contentStream.setFont(font, 16);
-            contentStream.newLineAtOffset(50, startY - 20);
+            contentStream.newLineAtOffset(27, startY - 20);
             contentStream.showText("Festivalort");
             contentStream.endText();
 
             // Datum und Uhrzeit
             contentStream.beginText();
             contentStream.setFont(font, 12);
-            contentStream.newLineAtOffset(50, startY - 40);
+            contentStream.newLineAtOffset(27, startY - 40);
             contentStream.showText("Freitag, 01.01.2001 20:00 Uhr");
             contentStream.endText();
 
             // Preis des Tickets
             contentStream.beginText();
             contentStream.setFont(font, 16);
-            contentStream.newLineAtOffset(50, startY -60);
-            contentStream.showText("Preis: 59,99EUR");
+            contentStream.newLineAtOffset(27, startY -60);
+            contentStream.showText(String.valueOf(price) + " EUR");
             contentStream.endText();
 
             // Tickettyp
             contentStream.beginText();
             contentStream.setFont(font, 12);
-            contentStream.newLineAtOffset(50, startY - 80);
+            contentStream.newLineAtOffset(27, startY - 80);
             contentStream.showText(ticketTypeName);
             contentStream.endText();
 
