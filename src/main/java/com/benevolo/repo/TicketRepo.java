343a00ec57d517e3b280b3f9588076a494f20673
Andreas Ott <andreas.konrad.ott@googlemail.com>
2024-05-20 15:08:24 +0200

    no show analytic

--- src/main/java/com/benevolo/repo/TicketRepo.java
+++ src/main/java/com/benevolo/repo/TicketRepo.java
@@ -5,11 +5,10 @@ import com.benevolo.utils.TicketStatus;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import io.quarkus.panache.common.Parameters;
 import jakarta.enterprise.context.ApplicationScoped;
-import net.bytebuddy.asm.Advice;
+import jakarta.persistence.Query;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.UUID;
 
 @ApplicationScoped
 public class TicketRepo implements PanacheRepositoryBase<Ticket, String> {
@@ -25,6 +24,21 @@ public class TicketRepo implements PanacheRepositoryBase<Ticket, String> {
     public long countByDate(String eventId, LocalDate date) {
         return count("SELECT COUNT(*) FROM Ticket AS t, BookingItem AS bi, Booking AS b WHERE t.bookingItem = bi AND bi.booking = b AND b.eventId = :eventId AND DATE(b.bookedAt) = :date",
                 Parameters.with("eventId", eventId).and("date", date));
+    }
+
+
+    public synchronized long countByStatus(String eventId, TicketStatus status) {
+        return count("SELECT COUNT(*) FROM Ticket AS t, BookingItem AS bi, Booking AS b WHERE t.bookingItem = bi AND bi.booking = b AND b.eventId = :eventId AND t.status = :status",
+                Parameters.with("eventId", eventId).and("status", status));
+    }
+
+    public synchronized long countByStatus2(String eventId, TicketStatus status) {
+        Query query = getEntityManager().createQuery("SELECT COUNT(t) FROM Ticket AS t, " +
+                "BookingItem AS bi, Booking AS b WHERE t.bookingItem = bi AND bi.booking = b AND b.eventId = :eventId " +
+                "AND t.status = :status");
+        query.setParameter("eventId", eventId);
+        query.setParameter("status", status);
+        return (long) query.getSingleResult();
     }
 
 }
