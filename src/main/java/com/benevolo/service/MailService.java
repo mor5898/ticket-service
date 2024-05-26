ef158653f6b5bdfc66ae48c2999f39edb56f4143
Moritz Reindl <mor.reindl@gmail.com>
2024-05-26 14:26:03 +0200

    reworked mail service

--- src/main/java/com/benevolo/service/MailService.java
+++ src/main/java/com/benevolo/service/MailService.java
@@ -8,10 +8,12 @@ import io.quarkus.mailer.Mail;
 import io.quarkus.mailer.Mailer;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.apache.commons.io.FileUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 @ApplicationScoped
 public class MailService {
@@ -27,15 +29,19 @@ public class MailService {
    Booking booking = new Booking();
     Customer customer = new Customer();
 
-    public void sendEmail(PDDocument pdf) {
-        ByteArrayOutputStream ticketOuputStream = new ByteArrayOutputStream();
+    public void sendEmail(PDDocument pdf) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        pdf.save(outputStream);
+        pdf.close();
+
+        byte[] pdfData = outputStream.toByteArray();
 
         mailer.send(
-                Mail.withText("tow6220@thi.de",
-                        "Hier könnte ihr Betreff stehen",
-                        "Hier sollte ihr Ticket sein :)"
-                ).addAttachment("Ticket.pdf",
-                        ticketOuputStream.toByteArray(),
+                Mail.withText("mor5898@thi.de",
+                        "Tickets Benevolo Shop",
+                        "Ihre Tickets sind im Anhang zu finden. :)"
+                ).addAttachment("ticket.pdf",
+                        pdfData,
                         "application/pdf")
         );
     }
