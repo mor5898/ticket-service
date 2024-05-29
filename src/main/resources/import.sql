6ce056c1f16e646958ec05ce61771ed54331e407
Andreas Dinauer <andreas.j.dinauer@gmail.com>
2024-05-29 14:28:23 +0200

    bookings status for event finished

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -47,16 +47,19 @@ VALUES ('aa75b92f-510c-44de-9fa0-5523ba6d96c2', '849380', 'a.d@mail.de'),
        ('cc75b92f-510c-44de-9fa0-5523ba6d96c2', '849382', 'f.s@icloud.com');
 
 INSERT INTO booking(id, event_id, total_price, booked_at, customer_id)
-VALUES ('377f870c-5a29-46e4-ac86-706c0ff00dc2', '383f700f-5449-4e40-b509-bee0b5d139d6', 2300, '2024-05-12 10:34:12', 'aa75b92f-510c-44de-9fa0-5523ba6d96c2');
+VALUES ('377f870c-5a29-46e4-ac86-706c0ff00dc2', '383f700f-5449-4e40-b509-bee0b5d139d6', 2300, '2024-05-12 10:34:12', 'aa75b92f-510c-44de-9fa0-5523ba6d96c2'),
+       ('366f870c-5a29-46e4-ac86-706c0ff00dc2', '383f700f-5449-4e40-b509-bee0b5d139d6', 3350, '2024-05-15 10:34:12', 'cc75b92f-510c-44de-9fa0-5523ba6d96c2');
 
 INSERT INTO booking_item(id, quantity, ticket_type_id, booking_id)
 VALUES ('a4a83ecc-098c-445f-9d94-0c98b5c3de0b', 3, '223f700f-5449-4e40-b509-bee0b5d139d6', '377f870c-5a29-46e4-ac86-706c0ff00dc2'),
-       ('6379fd9f-a4d9-4828-a7a1-4bf89f36d0b6', 2, '223f700f-5449-4e40-b509-bee0b5d139d6', '377f870c-5a29-46e4-ac86-706c0ff00dc2');
+       ('6379fd9f-a4d9-4828-a7a1-4bf89f36d0b6', 2, '223f700f-5449-4e40-b509-bee0b5d139d6', '377f870c-5a29-46e4-ac86-706c0ff00dc2'),
+       ('b4a83ecc-098c-445f-9d94-0c98b5c3de0b', 3, '223f700f-5449-4e40-b509-bee0b5d139d6', '366f870c-5a29-46e4-ac86-706c0ff00dc2'),
+       ('b379fd9f-a4d9-4828-a7a1-4bf89f36d0b6', 2, '223f700f-5449-4e40-b509-bee0b5d139d6', '366f870c-5a29-46e4-ac86-706c0ff00dc2');
 
 INSERT INTO ticket(id, public_id, status, price, tax_rate, booking_item_id)
 VALUES ('6375b92f-510c-44de-9fa0-5523ba6d96c2', '654927', 'VALID', 2000, 19, 'a4a83ecc-098c-445f-9d94-0c98b5c3de0b'),
        ('1375b92f-510c-44de-9fa0-5523ba6d96c2', '354927', 'VALID', 2000, 19, 'a4a83ecc-098c-445f-9d94-0c98b5c3de0b'),
-       ('2375b92f-510c-44de-9fa0-5523ba6d96c2', '454927', 'VALID', 2000, 19, 'a4a83ecc-098c-445f-9d94-0c98b5c3de0b'),
+       ('c375b92f-510c-44de-9fa0-5523ba6d96c2', '454927', 'VALID', 2000, 19, 'a4a83ecc-098c-445f-9d94-0c98b5c3de0b'),
        ('3375b92f-510c-44de-9fa0-5523ba6d96c2', '954927', 'VALID', 2000, 19, 'a4a83ecc-098c-445f-9d94-0c98b5c3de0b'),
        ('4375b92f-510c-44de-9fa0-5523ba6d96c2', '234927', 'VALID', 2000, 19, 'a4a83ecc-098c-445f-9d94-0c98b5c3de0b'),
        ('5375b92f-510c-44de-9fa0-5523ba6d96c2', '984927', 'REDEEMED', 2000, 19, 'a4a83ecc-098c-445f-9d94-0c98b5c3de0b'),
@@ -64,10 +67,14 @@ VALUES ('6375b92f-510c-44de-9fa0-5523ba6d96c2', '654927', 'VALID', 2000, 19, 'a4
        ('8375b92f-510c-44de-9fa0-5523ba6d96c2', '544927', 'REDEEMED', 2000, 19, 'a4a83ecc-098c-445f-9d94-0c98b5c3de0b'),
        ('9375b92f-510c-44de-9fa0-5523ba6d96c2', '144927', 'REDEEMED', 2000, 19, '6379fd9f-a4d9-4828-a7a1-4bf89f36d0b6'),
        ('0375b92f-510c-44de-9fa0-5523ba6d96c2', '154927', 'CANCELLED', 2000, 19, '6379fd9f-a4d9-4828-a7a1-4bf89f36d0b6'),
-       ('a375b92f-510c-44de-9fa0-5523ba6d96c2', '834927', 'CANCELLED', 2000, 19, '6379fd9f-a4d9-4828-a7a1-4bf89f36d0b6'),
-       ('b375b92f-510c-44de-9fa0-5523ba6d96c2', '824927', 'CANCELLED', 2000, 19, '6379fd9f-a4d9-4828-a7a1-4bf89f36d0b6'),
-       ('c375b92f-510c-44de-9fa0-5523ba6d96c2', '894927', 'CANCELLED', 2000, 19, '6379fd9f-a4d9-4828-a7a1-4bf89f36d0b6'),
-       ('d375b92f-510c-44de-9fa0-5523ba6d96c2', '894927', 'CANCELLED', 2000, 19, '6379fd9f-a4d9-4828-a7a1-4bf89f36d0b6');
+       ('b375b92f-510c-44de-9fa0-5523ba6d96c3', '834927', 'CANCELLED', 2000, 19, '6379fd9f-a4d9-4828-a7a1-4bf89f36d0b6'),
+       ('b376b92f-510c-44de-9fa0-5523ba6d96c3', '824927', 'CANCELLED', 2000, 19, '6379fd9f-a4d9-4828-a7a1-4bf89f36d0b6'),
+       ('c375b92f-510c-44de-9fa0-5523ba6d96c3', '894927', 'CANCELLED', 2000, 19, '6379fd9f-a4d9-4828-a7a1-4bf89f36d0b6'),
+       ('d375b92f-510c-44de-9fa0-5523ba6d96c3', '894927', 'CANCELLED', 2000, 19, '6379fd9f-a4d9-4828-a7a1-4bf89f36d0b6'),
+       ('a278b92f-510c-44de-9fa0-5523ba6d96c3', '134926', 'CANCELLED', 2000, 19, 'b4a83ecc-098c-445f-9d94-0c98b5c3de0b'),
+       ('a375b92f-510c-44de-9fa0-5523ba6d96c3', '124926', 'CANCELLED', 2000, 19, 'b4a83ecc-098c-445f-9d94-0c98b5c3de0b'),
+       ('b478b92f-510c-44de-9fa0-5523ba6d96c3', '194926', 'CANCELLED', 2000, 19, 'b379fd9f-a4d9-4828-a7a1-4bf89f36d0b6'),
+       ('a575b92f-510c-44de-9fa0-5523ba6d96c3', '194926', 'CANCELLED', 2000, 19, 'b379fd9f-a4d9-4828-a7a1-4bf89f36d0b6');
 
 
 INSERT INTO booking(id, event_id, total_price, booked_at, customer_id)
